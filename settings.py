3034817bfe3685e5514d40d00b8d8a5055750727
DonHyon <dhyon@hotmail.co.uk>
2025-05-22 20:29:02 +0100

    settings class made for background color

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings: 
+    """A class to store all settings for Alien Invasion.""" 
+
+    def __init__(self): 
+        """Game's settings initialized.""" 
+        # Screen settings 
+        self.screen_width = 960 
+        self.screen_height = 540 
+        self.bg_color = (0, 12, 25) 
+    
