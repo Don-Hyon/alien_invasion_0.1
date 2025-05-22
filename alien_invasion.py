cab33809145540f0c5d3a90ec3d602e7ff11628d
DonHyon <dhyon@hotmail.co.uk>
2025-05-22 21:24:30 +0100

    Simplified AlienInvasion class

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,17 +20,22 @@ class AlienInvasion:
     def run_game(self): 
         """Start the main loop for the game.""" 
         while True: 
-            # Watch for keyboard and mouse events/inputs. 
-            for event in pygame.event.get(): 
-                if event.type == pygame.QUIT: 
-                    sys.exit() 
+            self._check_events() 
+            self._update_screen() 
             
-            # Redraw the screen during each pass through the loop. 
-            self.screen.fill(self.settings.bg_color) 
-            self.ship.blitme() 
+    def _check_events(self): 
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                sys.exit() 
+
+    def _update_screen(self): 
+        """Updates inages on screen, and flip to new screen.""" 
+        self.screen.fill(self.settings.bg_color) 
+        self.ship.blitme() 
             
-            # Make the most recently drawn screen visible. 
-            pygame.display.flip() 
+        # Make the most recently drawn screen visible. 
+        pygame.display.flip() 
 
 if __name__ == '__main__': 
     #Make a game instance, and run game. 
