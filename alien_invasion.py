f37a1b3c05c3170b1bdd77f49e180d7bb5f8a0ad
DonHyon <dhyon@hotmail.co.uk>
2025-05-23 11:45:35 +0100

    Ship's basic movement refactored and by extention sorted for now.

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,24 +25,32 @@ class AlienInvasion:
             self._update_screen() 
             
     def _check_events(self): 
-        """Respond to keypresses and mouse events."""
+        """Respond to keypresses and mouse events.""" 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                 sys.exit() 
-
             elif event.type == pygame.KEYDOWN: 
-                if event.key == pygame.K_RIGHT: 
-                    # Move ship to the right. 
-                    self.ship.moving_right = True  
-                elif event.key == pygame.K_LEFT: 
-                    # Move ship to the left. 
-                    self.ship.moving_left = True 
-            
+                self._check_keydown_events(event) 
             elif event.type == pygame.KEYUP: 
-                if event.key == pygame.K_RIGHT: 
-                    self.ship.moving_right = False 
-                elif event.key == pygame.K_LEFT: 
-                    self.ship.moving_left = False 
+                self._check_keyup_events(event) 
+    
+    def _check_keydown_events(self, event): 
+        """Respond to key presses.""" 
+        if event.key == pygame.K_RIGHT: 
+            # Move ship to the right. 
+            self.ship.moving_right = True 
+        elif event.key == pygame.K_LEFT: 
+            # Move ship to the left. 
+            self.ship.moving_left = True 
+        elif event.key == pygame.K_q: 
+            sys.exit() 
+    
+    def _check_keyup_events(self, event): 
+        """Respond to key releases.""" 
+        if event.key == pygame.K_RIGHT: 
+            self.ship.moving_right = False 
+        elif event.key == pygame.K_LEFT: 
+            self.ship.moving_left = False 
 
     def _update_screen(self): 
         """Updates inages on screen, and flip to new screen.""" 
