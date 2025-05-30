8517d6c27b953f55f24e28a9187c1066e0182748
DonHyon <dhyon@hotmail.co.uk>
2025-05-30 17:29:58 +0100

    Variable settings and Static settings defined and sorted.

--- settings.py
+++ settings.py
@@ -2,25 +2,34 @@ class Settings:
     """A class to store all settings for Alien Invasion.""" 
 
     def __init__(self): 
-        """Game's settings initialized.""" 
+        """Game's static settings initialized.""" 
         # Screen settings 
         self.screen_width = 960 
         self.screen_height = 960 
         self.bg_color = (0, 12, 25) 
 
         # Ship settings 
-        self.ship_speed = 0.5 
         self.ship_limit = 2 
 
         # Bullet settings 
-        self.bullet_speed = 5 
         self.bullet_width = 5 
         self.bullet_height = 25 
         self.bullet_color = (125, 249, 255) 
         self.bullets_allowed = 2000 
 
         # Alien settings 
-        self.alien_speed = 0.050 
         self.fleet_drop_speed = 20 
-        # fleet_direction of 1 represents right; -1 represnts left. 
+
+        # How quickly the game speeds up 
+        self.speedup_scale = 1.10 
+
+        self.initialize_dynamic_settings() 
+    
+    def initialize_dynamic_settings(self): 
+        """Init settings that change throughout the game"""
+        self.ship_speed = 0.5 
+        self.bullet_speed = 5 
+        self.alien_speed = 0.10 
+
+        # Fleet_direction of 1 represents right; -1 represnts left. 
         self.fleet_direction = 1 
