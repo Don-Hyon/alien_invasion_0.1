34e6b06d4f51a20c4e12e33bc12200b5e2ec52a7
DonHyon <dhyon@hotmail.co.uk>
2025-05-30 17:54:31 +0100

    Game now resets after game over state.

--- alien_invasion.py
+++ alien_invasion.py
@@ -62,7 +62,8 @@ class AlienInvasion:
         """Start new game when player clicks 'Play'""" 
         button_clicked = self.play_button.rect.collidepoint(mouse_pos) 
         if button_clicked and not self.stats.game_active: 
-            # Reset the game stats. 
+            # Reset the game settings. 
+            self.settings.initialize_dynamic_settings() 
             self.stats.reset_stats() 
             self.stats.game_active = True 
 
